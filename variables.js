d2573c9d57d4c7ea579588a5e363fc6a33237593
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2021-07-15 21:34:00 +0300

    Variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+console.log('Labas pasauli!')
+
+let firstMessage;
+firstMessage = 100;
+console.log(firstMessage);
+
+let secondMessage = 150;
+console.log(secondMessage);
+
+let $thirdMessage = 155;
+console.log($thirdMessage);
