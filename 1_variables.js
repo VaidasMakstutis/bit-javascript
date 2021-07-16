2adc83b74c0c859bc638f40dc863551251a4c69b
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2021-07-16 22:23:49 +0300

    Updated variables

---
+++ 1_variables.js
@@ -0,0 +1,22 @@
+console.log('Labas pasauli!')   //string
+
+let firstMessage;
+firstMessage = 100;  //number
+console.log(firstMessage);
+
+let secondMessage = 150;   //number
+console.log(typeof secondMessage);
+
+let $thirdMessage = 'automobilis';   //string
+console.log(typeof $thirdMessage);
+
+let message = true;   //boolean
+console.log(typeof message);
+console.log(message);
+
+let messageNull = null;   // niekas
+console.log(messageNull);
+console.log(typeof messageNull);
+
+let messageUndefined;   // nepriskirta / neatpazinta reiksme
+console.log(messageUndefined);
