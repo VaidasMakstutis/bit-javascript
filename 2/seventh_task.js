8bbe5bf9c92ce453906d94d8085e4b2d0b2141be
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2021-07-19 22:07:44 +0300

    Corrected seventh task

--- 2/seventh_task.js
+++ 2/seventh_task.js
@@ -11,88 +11,30 @@ let a = rand(0, 100);
 let b = rand(0, 100);
 let c = rand(0, 100);
 let initialAverage;  //Pradine vidurkio reiksme
-let average1;   //Rezultatas atemus reiksmes mazesnes uz 10
-let average2;   //Rezultatas atemus reiksmes didesnes uz 90
 
 console.log('Sugeneruoti atsitiktiniai skaiciai: ' + a, b, c);
 initialAverage = (a + b + c) / 3;
 console.log('Pradine vidurkio reiksme: ' + initialAverage.toFixed(2));
 
-if (a < 10 && b < 10 && c < 10) {
-    average1 = initialAverage - a - b - c;
-    console.log('Vidurkis atemus reiksmes mazesnes uz 10: ' + average1.toFixed(2));
-}
-else if (a < 10 && b < 10 && c > 10) {
-    average1 = initialAverage - a - b;
-    console.log('Vidurkis atemus reiksmes mazesnes uz 10: ' + average1.toFixed(2));
-}
+let kaDalinam = initialAverage;
+let daliklis = 3;
 
-else if (a < 10 && b > 10 && c < 10) {
-    average1 = initialAverage - a - c;
-    console.log('Vidurkis atemus reiksmes mazesnes uz 10: ' + average1.toFixed(2));
+if (a <= 10 || a >= 90) {
+    kaDalinam -= a;
+    daliklis--;
 }
 
-else if (a > 10 && b < 10 && c < 10) {
-    average1 = initialAverage - b - c;
-    console.log('Vidurkis atemus reiksmes mazesnes uz 10: ' + average1.toFixed(2));
+if (b <= 10 || b >= 90) {
+    kaDalinam -= b;
+    daliklis--;
 }
 
-else if (a < 10 && b > 10 && c > 10) {
-    average1 = initialAverage - a;
-    console.log('Vidurkis atemus reiksmes mazesnes uz 10: ' + average1.toFixed(2));
+if (c <= 10 || c >= 90) {
+    kaDalinam -= c;
+    daliklis--;
 }
-
-else if (a > 10 && b < 10 && c > 10) {
-    average1 = initialAverage - b;
-    console.log('Vidurkis atemus reiksmes mazesnes uz 10: ' + average1.toFixed(2));
-}
-
-else if (a > 10 && b > 10 && c < 10) {
-    average1 = initialAverage - c;
-    console.log('Vidurkis atemus reiksmes mazesnes uz 10: ' + average1.toFixed(2));
-}
-
-else {
-    console.log('Nerasta reiksmiu mazesniu uz 10!');
-}
-
-
-
-
-if (a > 90 && b > 90 && c > 90) {
-    average2 = initialAverage - a - b - c;
-    console.log('Vidurkis atemus reiksmes didesnes uz 90: ' + average2.toFixed(2));
-}
-else if (a > 90 && b > 90 && c < 90) {
-    average2 = initialAverage - a - b;
-    console.log('Vidurkis atemus reiksmes didesnes uz 90: ' + average2.toFixed(2));
-}
-
-else if (a > 90 && b < 90 && c > 90) {
-    average2 = initialAverage - a - c;
-    console.log('Vidurkis atemus reiksmes didesnes uz 90: ' + average2.toFixed(2));
-}
-
-else if (a < 90 && b > 90 && c > 90) {
-    average2 = initialAverage - b - c;
-    console.log('Vidurkis atemus reiksmes didesnes uz 90: ' + average2.toFixed(2));
-}
-
-else if (a > 90 && b < 90 && c < 90) {
-    average2 = initialAverage - a;
-    console.log('Vidurkis atemus reiksmes didesnes uz 90: ' + average2.toFixed(2));
-}
-
-else if (a < 90 && b > 90 && c < 90) {
-    average2 = initialAverage - b;
-    console.log('Vidurkis atemus reiksmes didesnes uz 90: ' + average2.toFixed(2));
-}
-
-else if (a < 90 && b < 90 && c > 90) {
-    average2 = initialAverage - c;
-    console.log('Vidurkis atemus reiksmes didesnes uz 90: ' + average2.toFixed(2));
-}
-
-else {
-    console.log('Nerasta reiksmiu didesniu uz 90!');
-}
+// else {
+//     console.log('Galutine vidurkio reiksme: ' + kaDalinam / daliklis);
+// }
+console.log('Likutine vidurkio reiksme: ' + kaDalinam.toFixed(2), 'daliklis: ' + daliklis);
+console.log('Galutine vidurkio reiksme: ' + kaDalinam.toFixed(2) / daliklis);
