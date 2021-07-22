cfa2d8b81de6cc8cd2a763fcc582147698f68aed
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2021-07-22 21:51:51 +0300

    Updated tasks with do, while and for

--- 5/uzduotys.js
+++ 5/uzduotys.js
@@ -44,33 +44,33 @@ console.log('D:', D);
 
 //3 Uzduotis
 let pasveikinimas = 'labas';
-for (let ciklas = 0; ciklas < 5; ciklas++){
+for (let ciklas = 0; ciklas < 5; ciklas++) {
     console.log('Ciklas:', pasveikinimas);
 }
 
 //4 Uzduotis
 let pasveikinimas2 = D;
-for (let ciklas2 = 0; ciklas2 < 7; ciklas2++){
+for (let ciklas2 = 0; ciklas2 < 7; ciklas2++) {
     console.log('Ciklas:', pasveikinimas2);
 }
 
 //5 Uzduotis
 let pasveikinimas3 = D;
-for (let ciklas3 = 0; ciklas3 < D; ciklas3++){
+for (let ciklas3 = 0; ciklas3 < D; ciklas3++) {
     console.log('Ciklas tiek kartu kiek lygu D:', pasveikinimas3);
 }
 
 //6 Uzduotis
 let pasveikinimas4 = D;
 
-    if (pasveikinimas4 > 7) {
-        for (let ciklas4 = 0; ciklas4 < D; ciklas4++) {
+if (pasveikinimas4 > 7) {
+    for (let ciklas4 = 0; ciklas4 < D; ciklas4++) {
         console.log('Ciklas tiek kartu kiek lygu D jeigu didesnis uz 7:', pasveikinimas4);
-        }
     }
-    else {
-        console.log('Rezultatas mazesnis uz 7');
-    }
+}
+else {
+    console.log('Rezultatas mazesnis uz 7');
+}
 
 
 //7A Uzduotis
@@ -89,7 +89,7 @@ for (let B = 0; B < 5; B++) {
 }
 
 //7C Uzduotis
-let suma7C ='';
+let suma7C = '';
 
 for (let C = 0; C < 5; C++) {
     suma7C += rand(10, 20) + ' ';
@@ -99,7 +99,7 @@ console.log('Generuojamu reiksmiu suma stringe su tarpais:', suma7C);
 
 //7D Uzduotis
 
-let kint7D ='';
+let kint7D = '';
 let suma7D = 0;
 
 for (let D = 0; D < 5; D++) {
@@ -110,17 +110,95 @@ for (let D = 0; D < 5; D++) {
 kint7D += suma7D;
 console.log('Generuojamu reiksmiu suma stringe su tarpais ir suma:', kint7D);
 
-
-//8A Uzduotis
-
-let kint8A = 0;
+console.log('------------8A----------');
+let kint8A;
 
 do {
-(i = rand(10, 25));
-} while (i < 12);
+    kint8A = rand(10, 25);
+    console.log(kint8A);
+} while (kint8A >= 12);  //turim inversija
 
-kint8A = i;
-console.log('--Kint8A--', kint8A);
+console.log('------------8B----------');
 
 
-//8B Uzduotis
+let visoCiklu = 0;
+do {
+    kint8B = rand(10, 25);
+    visoCiklu++;
+} while (kint8B >= 12);
+console.log('Iteracijos: ' + visoCiklu, 'paskutine reiksme: ' + kint8B);
+
+
+let suma = 0;
+
+console.log('------------8C----------');
+
+do {
+    random = rand(10, 25);
+    console.log('Random: ', random);
+        suma += random > 18 ? 0 : random;
+} while (random >= 12);
+console.log(suma);
+
+
+console.log('------------8D----------');
+let suma1 = 0;
+let atmesta = 0;
+do {
+    random = rand(10, 25);
+    console.log('Random: ', random);
+    if (random <= 18) {
+        suma1 = suma1 + random;
+    }
+    else {
+        atmesta++;
+    }
+} while (random >= 12);
+console.log(suma, atmesta);
+
+console.log('------------8E----------');
+
+let lyginis = 0;
+let nelyginis = 0;
+
+do {
+    random = rand(10, 25);
+    console.log('Random: ', random);
+    if (random % 2 == 0) {
+        lyginis++;
+    }
+    else {
+        nelyginis++;
+    }
+} while (random >= 12);
+console.log(lyginis, nelyginis);
+
+console.log('------------8F----------');
+
+let visoMazuCiklu;
+do {
+    visoMazuCiklu = 0;
+    do {
+        random = rand(10, 25);
+        visoMazuCiklu++;
+    } while (random >= 12);  //inversija
+} while (visoMazuCiklu <= 20);  //inversija
+console.log('Mazu ciklu kiekis: ' + visoMazuCiklu);
+
+console.log('------------9A----------');
+
+let isridenta;
+let mCiklas = 0;
+let dCiklas = 0;
+do {
+    isridenta = rand(1, 6);
+
+    dCiklas++;
+
+    for (let i = 0; i < isridenta; i++) {
+        mCiklas++;
+    }
+
+} while (isridenta != 5);
+
+console.log('Didelis ciklu: ' + dCiklas, 'Mazu ciklu: ' + mCiklas);
