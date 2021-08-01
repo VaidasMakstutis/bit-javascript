4863902e13efc065d44632fc327bbc7d9776d8bc
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2021-08-01 22:05:33 +0300

    Finish array tasks

--- 11/11_paskaita.js
+++ 11/11_paskaita.js
@@ -163,7 +163,7 @@ for (let v = 0; v < ilgis; v++) {
 let maxReiksme = 0;
 
 for (let v = 0; v < a5.length; v++) {
-    
+
     if (maxReiksme < a5[v]) {
         maxReiksme = a5[v];
     }
@@ -178,4 +178,197 @@ for (let v = 0; v < a5.length; v++) {
 }
 
 console.log('Pinigine: ', a5.slice());
-console.log('Max reiksmiu kiekis: ', counterMax, maxReiksme);
+console.log('Max reiksme:', maxReiksme, 'Max reiksmiu kiekis: ', counterMax);
+
+
+console.log('---------------6 Uzduotis---------------');
+
+a5 = [];
+
+for (let i = 0; i < ilgis; i++) {
+    a5.push(rand(0, 10));
+
+    if (a5[i] == 0) {
+        a5[i] = i;
+    }
+}
+console.log('Pinigine su nuliniu reiksmiu vietos indeksais: ', a5.slice());
+
+
+
+console.log('---------------7 Uzduotis---------------');
+
+a5 = [];
+
+for (let i = 0; i < ilgis; i++) {
+    a5.push(rand(0, 10));
+}
+console.log('Pradine pinigine: ', a5.slice());
+
+while (a5.length < 30) {
+    a5.push(rand(0, 10));
+}
+console.log('Pinigine su pridetomis reiksmemis: ', a5.slice());
+
+
+
+console.log('---------------8 Uzduotis---------------');
+
+
+a5 = [];
+let a8Lower = [];   //masyvas reiksmems, kurios mazesnes arba lygios 2
+let a8Upper = [];   //masyvas reiksmems, kurios didesnes uz 2
+
+for (let z = 0; z < ilgis; z++) {
+    a5.push(rand(0, 10));
+    if (a5[z] > 2) {
+        a8Upper.push(a5[z]);
+    }
+    else {
+        a8Lower.push(a5[z]);
+    }
+}
+console.log('Pinigine: ', a5.slice());
+console.log('Monetos <=2:', a8Lower.slice());
+console.log('Popieriniai pinigai > 2:', a8Upper.slice());
+
+
+
+console.log('---------------9 Uzduotis---------------');
+
+let pinigine = [a8Lower, a8Upper];   //Masyvas is 2 dimensiju
+
+
+console.log(pinigine.slice());
+
+
+
+console.log('---------------10 Uzduotis---------------');
+
+let card = ['KIKA', 'Euro Vaistinė', 'Drogas', 'Ačiū', 'Lietuvos Geležinkeliai', 'Mano RIMI'];
+
+pinigine.push(card);
+
+console.log(pinigine.slice());
+
+
+
+console.log('---------------11 Uzduotis---------------');
+
+card.sort();
+console.log(pinigine.slice());
+
+
+
+console.log('---------------12 Uzduotis---------------');
+
+while (card.length < 20) {
+
+    if (rand(0, 1)) {
+        card.push('Visa');
+    }
+    else {
+        card.push('MasterCard');
+    }
+}
+// console.log(card.slice());
+console.log(pinigine.slice());
+
+
+console.log('---------------13 Uzduotis---------------');
+
+let counterMasterCard = 0;
+let counterVisa = 0;
+
+for (let i = 0; i < card.length; i++) {
+    if (card[i] == 'MasterCard') {
+        counterMasterCard++;
+    }
+    if (card[i] == 'Visa') {
+        counterVisa++;
+    }
+}
+
+console.log('MasterCard korteliu yra: ', counterMasterCard, 'Visa korteliu yra: ', counterVisa);
+
+if (counterMasterCard > counterVisa) {
+    console.log('MasterCard korteliu yra daugiau!');
+}
+else if (counterMasterCard < counterVisa) {
+    console.log('Visa korteliu yra daugiau!');
+}
+else {
+    console.log('MasterCard ir Visa korteliu yra po lygiai!')
+}
+
+
+
+console.log('---------------14 Uzduotis---------------');
+
+let tickets = [];  //Naujas masyvas loterijos bilietams
+
+pinigine.push(tickets);
+
+for (let j = 0; j < 10; j++) {
+    tickets.push(rand(1000000000, 9999999999));
+}
+// console.log(tickets);
+console.log(pinigine.slice());
+
+
+
+console.log('---------------15 Uzduotis---------------');
+
+// tickets.sort(function (a, b) { return b - a });
+tickets.sort();
+tickets.reverse();
+//console.log(tickets);
+console.log(pinigine.slice());
+
+
+
+console.log('---------------16 Uzduotis---------------');
+
+let amountA8Upper = 0;
+
+for (let k = 0; k < a8Upper.length; k++) {
+    amountA8Upper += a8Upper[k];
+}
+console.log(a8Upper);
+console.log('Pradine suma: ', amountA8Upper);
+
+
+while (amountA8Upper <= 500) {
+    let kupiura = rand(3, 10);
+    amountA8Upper += kupiura;
+    a8Upper.push(kupiura);
+}
+
+console.log('Galutine suma: ', amountA8Upper);
+
+
+
+console.log('---------------17 Uzduotis---------------');
+
+let counterWinTickets = 0;
+console.log(pinigine[3].slice());  //Tickets masyvas
+
+for (h = 0; h < tickets.length; h++) {
+    if (tickets[h] % 3 == 0) {
+        counterWinTickets++;
+        console.log('Jus laimejote!');
+    }
+    else {
+        console.log('Sis bilietas nelaimingas!');
+    }
+}
+console.log('Viso laimingu bilietu yra: ', counterWinTickets);
+
+
+console.log('---------------18 Uzduotis---------------');
+
+let pictures = ['šuo', 'katė', 'automobilis', 'namas', 'kiemas'];  //Naujas masyvas nuotraukoms
+
+pinigine.push(pictures);
+pictures.sort(function (a, b) { return a.length - b.length });
+console.log(pinigine.slice());
