b3fa07dd811c583031da23f3750225df13c3d064
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2021-08-23 22:42:33 +0300

    Baigiamoji - add new task in column

--- Baigiamoji_uzduotis/baigiamoji.js
+++ Baigiamoji_uzduotis/baigiamoji.js
@@ -1,12 +1,24 @@
 class Task {
 
     static tasks = [];
+    element;
 
     static start() {
         this.buttonSaveNewTask();
         this.addNewTaskButton();
         this.load();
     }
+
+    // static createNewTaskModal() {
+    //     const html = `
+    //     <h4> ${this.taskTitle} </h4>
+    //     <textarea> ${this.taskDescription} </textarea>
+    //     <br>
+    //     <button class="btn btn-primary" data-id="${this.id}"> X </button>
+    //     `;
+    //     // this.element.innerHTML = html;
+
+    // }
 
     static showCreateModal() {
         const modal = document.querySelector('#create-modal');
@@ -15,8 +27,8 @@ class Task {
         // modal.querySelector('.btn-primary').dataset.id = tasks.id;
     }
 
-    static hideCreateModal(id) {
-        const modal = document.querySelector('#' + id);
+    static hideCreateModal() {
+        const modal = document.querySelector('#create-modal');
         modal.style.display = 'none';
         modal.style.opacity = 0;
         delete modal.querySelector('.btn-secondary').dataset.id;
@@ -39,18 +51,24 @@ class Task {
     }
 
     static createTask(taskTitle, taskDescription, taskStatus) {
+        this.clearTask();
         this.tasks.push(new Task(taskTitle, taskDescription, taskStatus));
-        console.log(tasks);
+        this.renderTask();
+        this.save();
+        this.hideCreateModal();
     }
 
     static renderTask() {
         this.tasks.forEach(e => {
             e.render();
-            console.log(e);
+            // console.log(e);
         });
-        console.log(this.tasks);
+        // console.log(this.tasks);
     }
 
+    static clearTask() {
+        document.querySelector('.task-columns').innerHTML = '';
+    }
 
     static save() {
         const data = [];
@@ -74,7 +92,7 @@ class Task {
             localStorage.setItem('tasksApp', JSON.stringify([]));
         }
         JSON.parse(localStorage.getItem('tasksApp'))
-            .forEach(j => this.createTasksArray(j.taskTitle, j.taskDescription));
+            .forEach(j => this.createTask(j.taskTitle, j.taskDescription, j.taskStatus));
     }
 
 
@@ -87,24 +105,34 @@ class Task {
 
     render() {
         this.createNewTask();
-        this.createNewTaskHtml();
+        // this.createNewTaskModal();
     }
 
     createNewTask() {
-        this.element = document.createElement('div');
-        document.querySelector('#new-task').appendChild(this.element);
+        const html = document.createElement('div');
+        html.classList.add('new-task-card');
 
-    }
+        const body = document.createElement('div');
+        body.classList.add('task-content');
 
-    createNewTaskHtml() {
-        const html = `
-        <h4> ${this.taskTitle} </h4>
-        <textarea> ${this.taskDescription} </textarea>
-        <br>
-        <button class="btn btn-primary" data-id="${this.id}"> X </button>
-        `;
-        this.element.innerHTML = html;
+        // if (this.taskStatus == 'new') {
+        //     body.classList.add('new-task-card');
+        //     console.log('bla1: ', this.taskStatus);
+        // }
+        // else if (this.taskStatus == 'pending') {
+        //     body.classList.add('pending-task-card');
+        //     console.log('bla2: ', this.taskStatus);
+        // }
+        // else {
+        //     body.classList.add('done-task-card');
+        //     console.log('bla3: ', this.taskStatus);
+        // }
 
+        body.appendChild(document.createElement('h4')).appendChild(document.createTextNode(this.taskTitle));
+        body.appendChild(document.createElement('div')).appendChild(document.createTextNode(this.taskDescription));
+        // console.log(body);
+        html.appendChild(body);
+        document.querySelector('.task-columns').appendChild(html);
     }
 
 }
